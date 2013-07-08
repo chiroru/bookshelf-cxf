7b1c0f594204f25dc225ee1802b38c65c313b836
chiroru <chiroru_0130@yahoo.co.jp>
2013-07-08 23:38:28 +0900

    検索処理実装.

--- src/test/java/jp/ddo/chiroru/bookshelf/spring/BookshelfWebResourceTest.java
+++ src/test/java/jp/ddo/chiroru/bookshelf/spring/BookshelfWebResourceTest.java
@@ -1,5 +1,28 @@
 package jp.ddo.chiroru.bookshelf.spring;
 
+import jp.ddo.chiroru.bookshelf.Bookshelf;
+import jp.ddo.chiroru.bookshelf.util.ServletContainerResource;
+
+import org.apache.cxf.common.util.Base64Utility;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.junit.ClassRule;
+import org.junit.Test;
+
 public class BookshelfWebResourceTest {
 
+    private final static String ENDPOINT_ADDRESS = "http://127.0.0.1:8080/bookshelf";
+
+    @ClassRule
+    public static ServletContainerResource server = new ServletContainerResource();
+    
+    @Test
+    public void getBookshelfById() {
+        WebClient client = WebClient.create(ENDPOINT_ADDRESS);
+        client.accept("application/xml");
+        client.path("/bookshelfwebreousrce/bookslelves/{id}", 1);
+        client.header("Authorization", "Basic " + Base64Utility.encode(("fred" + ":" + "fredspassword").getBytes()));
+        System.out.println(client.get(String.class));
+        Bookshelf b = client.get(Bookshelf.class);
+        System.out.println("==========" + b.getId());
+    }
 }
---
+++ src/test/java/jp/ddo/chiroru/bookshelf/util/ServletContainerResource.java
@@ -0,0 +1,47 @@
+package jp.ddo.chiroru.bookshelf.util;
+
+import java.util.ResourceBundle;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.rules.ExternalResource;
+
+public class ServletContainerResource
+        extends ExternalResource {
+
+    private static String home;
+    private static String descriptor;
+    private static String contextPath;
+    private static int port;
+
+    private Server server;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("jetty");
+        home = bundle.getString("jetty.home");
+        descriptor = bundle.getString("jetty.descriptor");
+        contextPath = bundle.getString("jetty.contextPath");
+        port = Integer.valueOf(bundle.getString("jetty.port"));
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        server = new Server(port);
+        WebAppContext webapp = new WebAppContext();
+        webapp.setDescriptor(descriptor);
+        webapp.setContextPath(contextPath);
+        webapp.setResourceBase(home);
+        server.setHandler(webapp);
+        server.start();
+    }
+
+    @Override
+    protected void after() {
+        try {
+            server.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
