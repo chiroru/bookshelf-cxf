7878db9383697e0c0429cc02512e912292ad3868
chiroru <chiroru_0130@yahoo.co.jp>
2013-07-10 01:04:19 +0900

    クライアントにJacksonのプロバイダを適応.

--- src/test/java/jp/ddo/chiroru/bookshelf/spring/BookshelfWebResourceTest.java
+++ src/test/java/jp/ddo/chiroru/bookshelf/spring/BookshelfWebResourceTest.java
@@ -1,5 +1,7 @@
 package jp.ddo.chiroru.bookshelf.spring;
 
+import java.util.Arrays;
+
 import javax.ws.rs.core.MediaType;
 
 import jp.ddo.chiroru.bookshelf.util.ServletContainerResource;
@@ -7,6 +9,7 @@ import jp.ddo.chiroru.bookshelf.util.ServletContainerResource;
 import org.apache.cxf.common.util.Base64Utility;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.junit.ClassRule;
 import org.junit.Test;
 
@@ -19,8 +22,9 @@ public class BookshelfWebResourceTest {
     
     @Test
     public void getBookshelfById() {
-        WebClient client = WebClient.create(ENDPOINT_ADDRESS);
-        //client.accept("application/xml");
+        JacksonJsonProvider p = new JacksonJsonProvider();
+        WebClient client = WebClient.create(ENDPOINT_ADDRESS, Arrays.asList(p));
+        //client.accept(MediaType.APPLICATION_XML);
         client.accept(MediaType.APPLICATION_JSON);
         client.path("/bookshelfwebreousrce/bookslelves/{id}", 1);
         client.header("Authorization", "Basic " + Base64Utility.encode(("fred" + ":" + "fredspassword").getBytes()));
